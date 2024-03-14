4c7052046ac35d8df9ba8bb1128a78e215b05018
husniddin02 <study.husniddin@gmail.com>
2024-03-14 14:21:16 +0500

    .

--- frontapp/src/services/api.js
+++
@@ -1,9 +0,0 @@
-// src/services/api.js
-
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1/',
-});
-
-export default instance;
--- frontapp/src/utils/config.js
+++
@@ -1,3 +0,0 @@
-// src/utils/config.js
-
-export const API_BASE_URL = 'http://localhost:8000/api'; // Замените на ваш API URL
